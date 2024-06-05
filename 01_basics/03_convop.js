a8bab34cc4e4fd289910b9e4b8373ba287dba7b0
shailendra <64156759+shailendra-0@users.noreply.github.com>
2024-06-05 11:55:10 +0000

    operators  added

--- 01_basics/03_convop.js
+++ 01_basics/03_convop.js
@@ -26,4 +26,41 @@ let booleanIsLoggedIn= Boolean(isLoggedIn)
 let someNumber=33
 
 let stringNumber=String(someNumber)
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
+//********************       OPERATIONS         *****************************/
+
+
+let value =3
+let negValue= -value
+//console.log(negValue)
+
+
+//console.log(2+2);
+//console.log(-)
+//console.log(*)
+//console.log(/)
+
+let str1="helllo"
+let str2="shailu"
+
+let str3= str1+str2
+
+
+/* console.log(str3)
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+ 2+ "1");
+console.log(1+ 2 + "2"); */
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1=num2= num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
