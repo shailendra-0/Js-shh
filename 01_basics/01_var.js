e7100e01e429506a912c2208d003b5eedfab33a2
shailendra <64156759+shailendra-0@users.noreply.github.com>
2024-06-05 11:36:28 +0000

    operators

---
+++ 01_basics/01_var.js
@@ -0,0 +1,20 @@
+const accountid=8329
+let accountmail="asd@gmail.com"
+var accountpass="12324"
+
+accountcity="jaiour"
+
+let accountstate;
+
+/* we cannot change value of const
+
+we can change let and normal declaration */
+
+
+console.log(accountid)
+
+  /* prefer not to use var
+  because of issue in block scpoe and functional scope */
+
+console.table([accountid,accountmail,accountpass,accountcity,accountstate])
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+//code readability shoukd be high
+
+let name="shailu"
+let age=14
+
+let isloggedIn= false
+let state=null
+
+
+
+//number => 2 to power 53
+//bigint
+//string=""
+//boolean=false/true
+//null=> standalonr value
+//undefined=> 
+//symbol=> unique
+
+
+
+//object
+
+console.log(typeof "shailu");
+console.log(typeof null); //object
+
