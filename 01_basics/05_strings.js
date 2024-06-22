fe2cddf122fdc39fc9a9ddbd2fc1a62b45a373cd
shailendra <64156759+shailendra-0@users.noreply.github.com>
2024-06-22 12:18:38 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name ="shailu"
+
+const repoCount=5
+
+//console.log(name+repoCount+ " hi");
+
+
+console.log(`hello my name is ${name} and my rep count is ${repoCount}`);
+
+const gameName = new String('shailu')
+
+console.log(gameName);
+console.log(gameName.__proto__);
+
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('u'));
+
+const newString =gameName.substring(0,4)
+console.log(newString);
+
+//we cant satrt from -ve it starts from 0 only
+
+const anotherString= gameName.slice(-8,1)
+console.log(anotherString);
+
+const newStringOne=" sdk   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url= "http://shailendra.com//s%20cheruku"
+console.log(url.replace('%20','-'));
