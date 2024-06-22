2b2a17b5844fc33b039c36ab4e65ef223c661d0f
shailendra <64156759+shailendra-0@users.noreply.github.com>
2024-06-22 11:28:12 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,44 @@
+// Primitive
+
+//7 ytpes: Stirng.Number,Boolean,nuull,undefined ,symbol,BigInt
+
+const score=100
+const scoreValue= 100.3
+
+const isLoggedIn= false
+const outsideTemp=null
+let userEmail;
+
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id==anotherId);
+
+
+// Refrence(non Primitive)
+
+//Array,Objects, Functions
+
+const heros=["shailu", "bhavuk","ayush"]
+
+
+//object is a key value pair 
+
+let Obj={
+
+    name:"shailu",
+    age: 22 
+}
+
+
+const myfun = function(){
+    console.log("hello");
+}
+
+
+
+//to find data type
+
+console.log(typeof myfun);
+
