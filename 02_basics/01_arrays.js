761a743233015d5d88c72bb64ba14519478b9638
shailendra <64156759+shailendra-0@users.noreply.github.com>
2024-07-05 07:38:51 +0000

    arrays p1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,48 @@
+//array
+
+
+const myArr= [0,1,23,4,4,5,2]
+
+//console.log(myArr[0]);
+
+const Arr1= new Array(1,2,3,4,5)
+
+const AArr2= ["shailu","shailendra"]
+
+//array methods
+
+// myArr.push(3)
+// myArr.push(4)
+// myArr.pop()
+
+//myArr.unshift(9) //to insert in front
+
+// myArr.shift()
+
+
+// console.log(myArr.includes(4));
+
+// console.log(myArr.indexOf(23));
+
+// const newArr= myArr.join()
+
+// console.log(newArr);
+// console.log(myArr);
+
+
+//slice,splice
+
+console.log( "A",myArr);
+
+const myn1=myArr.slice(1,3)
+
+console.log(myn1);
+
+
+console.log("B",myArr);
+
+const myn2=myArr.splice(1,3)
+
+console.log(myn2);
+
+console.log("C",myArr)
