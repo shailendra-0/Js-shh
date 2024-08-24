7a99cdbb095414eff50c908f1e70009049a8e908
shailendra <64156759+shailendra-0@users.noreply.github.com>
2024-08-24 14:33:52 +0000

    flow

---
+++ 04_controlflow/one.js
@@ -0,0 +1,15 @@
+//if
+const temp=40
+
+const balance=1000
+
+if (balance>500) console.log("test");
+//implicit scope
+
+if (balance>500) console.log("test"), 
+console.log("test");
+//unreadble code, immature too  dont practrice putting comma
+
+//NESTING
+
+
