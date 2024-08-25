db42129924e2759b7c6e674d224637db316d6f22
shailendra <64156759+shailendra-0@users.noreply.github.com>
2024-08-25 09:11:18 +0000

    switches my notes

--- 04_controlflow/switch.js
+++ 04_controlflow/switch.js
@@ -6,3 +6,8 @@
 //   default:
 //     break;
 // }
+// falsy values     
+
+// false,0,-0.BigInt onabort,"",null,undefined NaN
+// truthy values
+// "0",'false'(anything inside quotes is true),"",[],{},function(){} (empty function)
