d0a20cf09ea71a91fa2f8dde30169c6d461e5b89
shailendra <64156759+shailendra-0@users.noreply.github.com>
2024-08-26 13:01:10 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,40 @@
+// const coding=['f','g','h','d']
+
+
+//  const val=coding.forEach( (item)=>{
+
+// console.log(item);
+// return item
+
+//  })
+//  console.log(val);
+ 
+/****** foreach dosent returns but filter returns the values*/
+
+ const myNum=[1,2,3,4,5,6,7,8]
+
+// const newNums= myNum.filter( (num)=>num>4)
+//  console.log(newNums);
+ 
+// const newNums= myNum.filter( (num)=>{
+
+//   // num>4 we cannot return like this beacuse explicit return refer arrow function here we have to put return 
+//   return num>4
+// })
+// console.log(newNums);
+
+/**********here we are now making foreach to return just like filter did */
+
+// const newNums=[]
+// myNum.forEach((num)=>{
+//   if(num>4)
+//   {
+//     newNums.push(num)
+
+//   }
+// })
+// console.log(newNums);
+
+
+
+
