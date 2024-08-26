ef68b88b551bf8e7467e5f48e8c07bd69ebf8921
shailendra <64156759+shailendra-0@users.noreply.github.com>
2024-08-26 13:18:59 +0000

    control flow

--- 04_controlflow/switch.js
+++ 04_controlflow/switch.js
@@ -11,3 +11,36 @@
 // false,0,-0.BigInt onabort,"",null,undefined NaN
 // truthy values
 // "0",'false'(anything inside quotes is true),"",[],{},function(){} (empty function)
+
+
+/*****HITESH GH********* */
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
